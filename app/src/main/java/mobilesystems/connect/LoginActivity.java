597e3b6042b19a2b58960e24197f4b8955effb87
Ching-Ya Huang <ch775@cornell.edu>
2014-11-05 10:36:06 -0500

    add Facebook api

---
+++ app/src/main/java/mobilesystems/connect/LoginActivity.java
@@ -0,0 +1,136 @@
+package mobilesystems.connect;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.LoginButton.UserInfoChangedCallback;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by yellow on 11/5/14.
+ */
+public class LoginActivity extends Activity {
+    private LoginButton loginBtn;
+    private TextView userName;
+
+    private UiLifecycleHelper uiHelper;
+    private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        uiHelper = new UiLifecycleHelper(this, statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_profile);
+
+        userName = (TextView) findViewById(R.id.user_name);
+        loginBtn = (LoginButton) findViewById(R.id.fb_login_button);
+        loginBtn.setUserInfoChangedCallback(new UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                if (user != null) {
+                    userName.setText("Hello, " + user.getName());
+                } else {
+                    userName.setText("You are not logged");
+                }
+            }
+        });
+
+//        try { // if hash key doesn't work!
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "mobilesystems.connect",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.i("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
+    }
+
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state,
+                         Exception exception) {
+            if (state.isOpened()) {
+                Log.i("FacebookSampleActivity", "Facebook session opened");
+            } else if (state.isClosed()) {
+                Log.i("FacebookSampleActivity", "Facebook session closed");
+            }
+        }
+    };
+
+    public boolean checkPermissions() {
+        Session s = Session.getActiveSession();
+        if (s != null) {
+            return s.getPermissions().contains("publish_actions");
+        } else
+            return false;
+    }
+
+    public void requestPermissions() {
+        Session s = Session.getActiveSession();
+        if (s != null)
+            s.requestNewPublishPermissions(new Session.NewPermissionsRequest(
+                    this, PERMISSIONS));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
+        uiHelper.onSaveInstanceState(savedState);
+    }
+
+}
