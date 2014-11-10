36a8e014541bd139c2a93d9652eb8d777fd189d7
Edward Arthur Bingham <eab242@cornell.edu>
2014-11-09 21:30:43 -0500

    Get an access token from moves so that we can use their API.

--- app/src/main/java/mobilesystems/connect/LoginActivity.java
+++ app/src/main/java/mobilesystems/connect/LoginActivity.java
@@ -1,16 +1,21 @@
 package mobilesystems.connect;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.HttpMethod;
 import com.facebook.Request;
@@ -21,11 +26,34 @@ import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 import com.facebook.widget.LoginButton.UserInfoChangedCallback;
+import com.google.gson.Gson;
 
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.util.Log;
+import org.apache.http.client.methods.HttpGet;
 
 /**
  * Created by yellow on 11/5/14.
@@ -36,6 +64,26 @@ public class LoginActivity extends Activity {
 
     private UiLifecycleHelper uiHelper;
     private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
+
+    // Moves
+
+    private static final String TAG = "Connect";
+    private static final String CLIENT_ID = "BHJJXLewp3VFBhgOY1T7NVlyXGsOtMF1";
+    private static final String REDIRECT_URI = "http://www.sol-union.com/moves/auth.php";
+    private static final int REQUEST_AUTHORIZE = 1;
+    private static final int RECEIVE_AUTHORIZE = 2;
+
+    public class MovesAccessToken {
+        MovesAccessToken() {}
+
+        String access_token;
+        String token_type;
+        long expires_in;
+        String refresh_token;
+        String user_id;
+    }
+
+    MovesAccessToken accessToken = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -125,12 +173,114 @@ public class LoginActivity extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
         uiHelper.onActivityResult(requestCode, resultCode, data);
 
+        switch (requestCode) {
+            case REQUEST_AUTHORIZE:
+                Uri resultUri = data.getData();
+                if (resultCode == RESULT_OK) {
+                    doReceiveAuthInApp(resultUri);
+                }
+                Toast.makeText(this,
+                        (resultCode == RESULT_OK ? "Authorized: " : "Failed: ")
+                                + data.getDataString(), Toast.LENGTH_LONG).show();
+                break;
+            case RECEIVE_AUTHORIZE:
+                Toast.makeText(this,
+                        (resultCode == RESULT_OK ? "Authorized: " : "Failed: ")
+                                + data.getDataString(), Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedState) {
         super.onSaveInstanceState(savedState);
         uiHelper.onSaveInstanceState(savedState);
+    }
+
+    /**
+     * App-to-app. Creates an intent with data uri starting moves://app/authorize/xxx (for more
+     * details, see documentation link below) to be handled by Moves app. When Moves receives this
+     * Intent it opens up a dialog asking for user to accept the requested permission for your app.
+     * The result of this user interaction is delivered to
+     * {@link #onActivityResult(int, int, android.content.Intent) }
+     *
+     * @see https://dev.moves-app.com/docs/api
+     */
+    private void doRequestAuthInApp() {
+        Uri uri = createAuthUri("moves", "app", "/authorize").build();
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        try {
+            startActivityForResult(intent, REQUEST_AUTHORIZE);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "Moves app not installed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class GetHTTP extends AsyncTask<Uri, Void, String> {
+        protected String doInBackground(Uri... urls) {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(urls[0].toString());
+            StringBuilder builder = new StringBuilder();
+
+            try {
+                // defaultHttpClient
+                HttpResponse response = httpClient.execute(httpGet);
+                StatusLine statusLine = response.getStatusLine();
+                int statusCode = statusLine.getStatusCode();
+                if (statusCode == 200) {
+                    HttpEntity entity = response.getEntity();
+                    InputStream content = entity.getContent();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(content));
+                    String line;
+                    while ((line = reader.readLine()) != null)
+                        builder.append(line);
+                    return builder.toString();
+                } else
+                    Log.e("Getter", "Failed get address");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            LoginActivity.this.doReceiveTokenInApp(result);
+        }
+    }
+
+    private void doReceiveAuthInApp(Uri uri) {
+        new GetHTTP().execute(uri);
+    }
+
+    private void doReceiveTokenInApp(String token) {
+        Gson gson = new Gson();
+        accessToken = gson.fromJson(token, MovesAccessToken.class);
+        Toast.makeText(LoginActivity.this, accessToken.access_token, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Helper method for building a valid Moves authorize uri.
+     */
+    private Uri.Builder createAuthUri(String scheme, String authority, String path) {
+        return new Uri.Builder()
+                .scheme(scheme)
+                .authority(authority)
+                .path(path)
+                .appendQueryParameter("client_id", CLIENT_ID)
+                .appendQueryParameter("redirect_uri", REDIRECT_URI)
+                .appendQueryParameter("scope", "location activity")
+                .appendQueryParameter("state", String.valueOf(SystemClock.uptimeMillis()));
+    }
+
+    public void connectMoves(View view) {
+        doRequestAuthInApp();
     }
 
 }
--- app/src/main/java/mobilesystems/connect/Profile.java
+++ app/src/main/java/mobilesystems/connect/Profile.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.Intent;
+import android.view.View;
 
 public class Profile extends Activity {
 
@@ -27,10 +28,15 @@ public class Profile extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId())
+        {
+            case R.id.action_settings:
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
